4972825740500441a518efcc2b9fbca7347562d0
unknown <aaron.campbell.ncfca@gmail.com>
2014-04-28 12:13:52 -0700

    initial commit

---
+++ src/com/example/ch4/ContactMapActivity.java
@@ -0,0 +1,61 @@
+package com.example.ch4;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ContactMapActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact_map);
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+	}
+
+	private void initListButton() {
+        ImageButton list = (ImageButton) findViewById(R.id.m_imageButtonList);
+        list.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+    			Intent intent = new Intent(ContactMapActivity.this, ContactListActivity.class);
+    			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    			startActivity(intent);
+            }
+        });
+	}
+	
+	private void initMapButton() {
+        ImageButton list = (ImageButton) findViewById(R.id.m_imageButtonMap);
+        list.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+    			Intent intent = new Intent(ContactMapActivity.this, ContactMapActivity.class);
+    			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    			startActivity(intent);
+            }
+        });
+	}
+	
+	private void initSettingsButton() {
+        ImageButton list = (ImageButton) findViewById(R.id.m_imageButtonSettings);
+        list.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+    			Intent intent = new Intent(ContactMapActivity.this, ContactSettingsActivity.class);
+    			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    			startActivity(intent);
+            }
+        });
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.contact_map, menu);
+		return true;
+	}
+
+}
---
+++ src/com/example/ch4/ContactSettingsActivity.java
@@ -0,0 +1,63 @@
+package com.example.ch4;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class ContactSettingsActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_contact_settings);
+        initListButton();
+        initMapButton();
+        initSettingsButton();
+		
+	}
+
+	private void initListButton() {
+        ImageButton list = (ImageButton) findViewById(R.id.s_imageButtonList);
+        list.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+    			Intent intent = new Intent(ContactSettingsActivity.this, ContactListActivity.class);
+    			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    			startActivity(intent);
+            }
+        });
+	}
+	
+	private void initMapButton() {
+        ImageButton list = (ImageButton) findViewById(R.id.s_imageButtonMap);
+        list.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+    			Intent intent = new Intent(ContactSettingsActivity.this, ContactMapActivity.class);
+    			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    			startActivity(intent);
+            }
+        });
+	}
+	
+	private void initSettingsButton() {
+        ImageButton list = (ImageButton) findViewById(R.id.s_imageButtonSettings);
+        list.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+    			Intent intent = new Intent(ContactSettingsActivity.this, ContactSettingsActivity.class);
+    			intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+    			startActivity(intent);
+            }
+        });
+	}
+	
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.contact_settings, menu);
+		return true;
+	}
+
+	
+}
